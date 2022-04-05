d0fd0f0c5baf9fe626e62f82507153612ca40b03
2DN <hunes11@gmail.com>
2022-04-05 20:18:45 +0900

    3.6 Getter & Setter
    Event listen / getter & setter

--- app.js
+++ app.js
@@ -2,39 +2,16 @@
 
 const h1 = document.querySelector("div.hello:first-child h1");
 
+// 클릭시 컬러 변경 블루 - 레드
 function handleTitleClick() {
-  h1.style.color = "blue";
-}
-
-function handleMouseEnter() {
-  h1.innerText = "Mouse is here";
-}
-
-function handleMouseLeave() {
-  h1.innerText = "Mouse is gone";
-}
-
-function handleWinddowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-  alert("copier!");
-}
-
-function handleWindowOffline() {
-  alert("sos I need wifi");
-}
-
-function handleWindowOnline() {
-  alert("You have wifi");
+  const currentColor = h1.style.color; //getter
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
+  h1.style.color = newColor; //setter
 }
 
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWinddowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
